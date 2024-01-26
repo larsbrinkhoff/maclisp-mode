abb6320dd71883f04b83f88c4f0f1472bb5d2551
Lars Brinkhoff <lars@nocrew.org>
2024-01-26 07:32:29 +0100

    Minor mode for PDP-6 era Maclisp syntax.

--- maclisp-mode.el
+++ maclisp-mode.el
@@ -3,3 +3,20 @@
 
 (modify-syntax-entry ?\\ "_" maclisp-mode-syntax-table)
 (modify-syntax-entry ?/ "\\" maclisp-mode-syntax-table)
+
+(defvar pdp6-lisp-syntax-table
+  (let ((table (make-syntax-table maclisp-mode-syntax-table)))
+    (modify-syntax-entry ?' "_" table)
+    (modify-syntax-entry ?` "_" table)
+    (modify-syntax-entry ?, "_" table)
+    (modify-syntax-entry ?@ "_" table)
+    (modify-syntax-entry ?\" "_" table)
+    table))
+
+(define-minor-mode pdp6-lisp-mode
+  "Minor mode for PDP-6 LISP"
+  :init-value nil
+  :lighter " PDP-6"
+  (set-syntax-table (if pdp6-lisp-mode
+                        pdp6-lisp-syntax-table
+                        maclisp-mode-syntax-table)))
